05399800b735969d8e578f5e56652b168491d4f2
AndrusovN <andrusov.n@gmail.com>
2020-06-25 00:38:08 +0300

    Errors and warnings creation

--- McCabeCM.py
+++ McCabeCM.py
@@ -22,8 +22,22 @@ if(len(sys.argv) > 3):
 
 
 interestingComplexity = 0
+
+makeErrors = False
 if(len(sys.argv) > 4):
-    interestingComplexity = int(sys.argv[4])
+    if(sys.argv[4].isnumeric()):
+        interestingComplexity = int(sys.argv[4])
+    else:
+        if(sys.argv[4] == '/err'):
+            makeErrors = True
+        elif(sys.argv[4] == '/nerr'):
+            makeErrors = False
+
+if(len(sys.argv) > 5):
+    if (sys.argv[5] == '/err'):
+        makeErrors = True
+    elif (sys.argv[5] == '/nerr'):
+        makeErrors = False
 
 filenames = []
 
@@ -43,19 +57,29 @@ for filename in filenames:
 
 expression = r"[\w\d<>:\[\]]+[*]{0,1} [\w\d:]+\((?:[\w\d<>:*\[\]]+ [\w\d<>:*\[\]]+(?:,[ \n]*){0,1})*\)[\n]*\{"
 keywords = [r"for", r"while", r"do", r"if", r"[&]{2}", r"[|]{2}", r"switch", r"throw", r"catch", r"goto"]
+tresholds = [-1, 5, 10, 20, 10000000000]
+warningIndex = 2
+errorIndex = 3
+recomendations = ["", "are very good", "are recommended to rewrite", "you must rewrite later", "you must rewrite now"]
+
+errorText = '#error "This method is too complex to read! Reconstruct it"'
+warningText = '#warning "This method is complex to read! Maybe it is good to reconstruct it'
 
 results = {}
 for filename in suitableFiles:
     file = open(filename, 'r', encoding='utf-8')
     data = file.read()
     #print("\n\n\nFrom file " + filename)
+    data = data.replace("\n" + warningText + "\n", "")
+    data = data.replace("\n" + errorText + "\n", "")
 
 
     #([\w\d<>:\[\]]+[*]{0,1} [*]{0,1}[\w\d<>:\[\]]+(,[ \n]){0,1})*
     methods = re.split(expression, data)
     names = re.findall(expression, data)
-
-
+    resultData = ""
+    if(len(methods) > 0):
+        resultData = methods[0]
     for i in range(1, len(methods)):
         method = methods[i]
         complexity = 1
@@ -65,14 +89,20 @@ for filename in suitableFiles:
             #print(method)
             #print(re.findall(keyword, method))
         results[names[i-1]] = complexity
-
-
-            #print("complexity = " + str(results[methodname]) + "\n")
+        if(makeErrors):
+            if (complexity > tresholds[errorIndex]):
+                resultData += "\n" + errorText + "\n"
+            elif(complexity > tresholds[warningIndex]):
+                resultData += "\n" + warningText + "\n"
+        resultData += names[i-1]
+        resultData += methods[i]
 
     file.close()
+    writeFile = open(filename, 'w', encoding='utf-8')
+    writeFile.write(resultData)
+    writeFile.close()
 
-tresholds = [-1, 5, 10, 20, 10000000000]
-recomendations = ["", "are very good", "are recommended to rewrite", "you must rewrite later", "you must rewrite now"]
+
 
 for i in range(1, len(tresholds)):
     if(tresholds[i] > interestingComplexity):
