60c76fcb4aa3ae8d91649889ebb2b7fc054a51b7
AndrusovN <andrusov.n@gmail.com>
2020-06-24 14:54:44 +0300

    initial commit

---
+++ McCabeCM.py
@@ -0,0 +1,85 @@
+import re
+import os
+import sys
+
+folder = ""
+
+if(len(sys.argv) > 1):
+    folder = sys.argv[1]
+
+extention = ""
+
+if(len(sys.argv) > 2):
+    extention = sys.argv[2]
+
+recursiveley = True
+
+if(len(sys.argv) > 3):
+    if(sys.argv[3] == '/nr'):
+        recursiveley = False
+    if(sys.argv[3] == '/r'):
+        recursiveley = True
+
+
+interestingComplexity = 0
+if(len(sys.argv) > 4):
+    interestingComplexity = int(sys.argv[4])
+
+filenames = []
+
+if(recursiveley):
+    for root, subdirs, files in os.walk(folder):
+        for filename in files:
+            filenames.append(root + '\\' + filename)
+else:
+    filenames = [(folder + '\\' + el) for el in os.listdir(folder)]
+
+
+suitableFiles = []
+
+for filename in filenames:
+    if(filename[-len(extention):] == extention):
+        suitableFiles.append(filename)
+
+expression = r"[\w\d<>:\[\]]+[*]{0,1} [\w\d:]+\((?:[\w\d<>:*\[\]]+ [\w\d<>:*\[\]]+(?:,[ \n]*){0,1})*\)[\n]*\{"
+keywords = [r"for", r"while", r"do", r"if", r"[&]{2}", r"[|]{2}", r"switch", r"case", r"throw", r"catch", r"goto"]
+
+results = {}
+for filename in suitableFiles:
+    file = open(filename, 'r')
+    data = file.read()
+    #print("\n\n\nFrom file " + filename)
+
+
+    #([\w\d<>:\[\]]+[*]{0,1} [*]{0,1}[\w\d<>:\[\]]+(,[ \n]){0,1})*
+    methods = re.split(expression, data)
+    names = re.findall(expression, data)
+
+
+    for i in range(1, len(methods)):
+        method = methods[i]
+        complexity = 1
+        for keyword in keywords:
+            complexity += len(re.findall(keyword, method))
+            #print("--------------------NEW METHOD--------------------------")
+            #print(method)
+            #print(re.findall(keyword, method))
+        results[names[i-1]] = complexity
+
+
+            #print("complexity = " + str(results[methodname]) + "\n")
+
+    file.close()
+
+tresholds = [-1, 5, 10, 20, 10000000000]
+recomendations = ["", "are very good", "are recommended to rewrite", "you must rewrite later", "you must rewrite now"]
+
+for i in range(1, len(tresholds)):
+    print('\n\nMethods that ' + recomendations[i] + '\n')
+    for methodname in results.keys():
+        if(tresholds[i-1] < results[methodname] <= tresholds[i]):
+            if(methodname[-2] == '\n'):
+                print(methodname[:-2])
+            else:
+                print(methodname[:-1])
+#print(suitableFiles)
